28fea3d9325d124fb0d2e5794f3d019fe8388568
josep <jmoll20950@iesjoanramis.org>
2024-10-24 16:18:07 +0200

    Comentarios javadoc añadidos

--- FuncionSuma.java
+++ FuncionSuma.java
@@ -1,15 +1,26 @@
 import java.util.Scanner;
 
 
+/**
+ * @version v1.0
+ * @author Miquel
+ * @see Para más información ver <a href="https://www.google.es/">Google</a>
+ */
 
 public class FuncionSuma {
-
-	
-	
+	/**
+	  * Variable para guardar el primer sumando
+	  */
 	private static int primerSumando;
-	
+	/**
+      * Variable para guardar el segundo sumando
+      * @param args
+      */
 	private static int segundoSumando; 
-	
+	/**
+      	* Funcion principal del programa
+      	* @param args El parametro habitual de la función main
+      	*/
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		
@@ -27,8 +38,14 @@ public class FuncionSuma {
 	}
 	
 	
-	
+	/**
+     	* Funcion que realiza la suma de los dos operadores
+     	* @param args El parametro habitual de la función main
+     	*/	
 	private static int suma (int sumando1, int sumando2) {
+		/**
+		 * @return Delvuelve el resultado de sumar los dos sumandos
+		 */
 		return sumando1-sumando2;
 	}
 
